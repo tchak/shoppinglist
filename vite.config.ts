8df4db1c39a20e53e34d89dbac7781eace97032f
Paul Chavard <github@paul.chavard.net>
2021-02-06 14:05:45 +0100

    Add builds for legacy browsers

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
 import reactRefresh from '@vitejs/plugin-react-refresh';
+import legacy from '@vitejs/plugin-legacy';
 import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
@@ -6,6 +7,9 @@ export default defineConfig({
   plugins: [
     reactRefresh({
       parserPlugins: ['classProperties', 'classPrivateProperties'],
+    }),
+    legacy({
+      targets: ['defaults', 'not IE 11'],
     }),
   ],
 });
