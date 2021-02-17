8cbbc9a72ffd2e63d1eff443a75e9a1fdc6dbba9
Paul Chavard <github@paul.chavard.net>
2021-02-17 20:44:44 +0100

    Add more food

--- src/data/food.ts
+++ src/data/food.ts
@@ -1,9 +1,10 @@
 import fruits from './fruits.json';
 import vegetables from './vegetables.json';
 import condiments from './condiments.json';
+import misc from './misc.json';
 
 const food: string[] = [
-  ...new Set([...fruits, ...vegetables, ...condiments].map(titleize)),
+  ...new Set([...fruits, ...vegetables, ...condiments, ...misc].map(titleize)),
 ];
 
 function titleize(input: string) {
