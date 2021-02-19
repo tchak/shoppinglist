a3a5e4d66c89c53aba579f89a97a836e6b16c19f
Paul Chavard <github@paul.chavard.net>
2021-02-19 17:37:13 +0100

    remove Transition untill its types are fixed

--- src/components/Notification.tsx
+++ src/components/Notification.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { HiX } from 'react-icons/hi';
 import { Alert } from '@reach/alert';
-import { Transition } from '@headlessui/react';
 
 export function Notification({
   isOpen,
@@ -13,15 +12,10 @@ export function Notification({
   text: string;
 }) {
   return (
-    <Transition
-      className="fixed inset-0 flex items-end justify-center px-4 py-6 pointer-events-none sm:p-6 sm:items-start sm:justify-end"
-      show={isOpen}
-      enter="transform ease-out duration-300 transition"
-      enterFrom="translate-y-2 opacity-0 sm:translate-y-0 sm:translate-x-2"
-      enterTo="translate-y-0 opacity-100 sm:translate-x-0"
-      leave="transition ease-in duration-100"
-      leaveFrom="opacity-100"
-      leaveTo="opacity-0"
+    <div
+      className={`fixed inset-0 flex items-end justify-center px-4 py-6 pointer-events-none sm:p-6 sm:items-start sm:justify-end ${
+        isOpen ? '' : 'hidden'
+      }`}
     >
       <Alert className="max-w-sm w-full bg-white shadow-lg rounded-lg pointer-events-auto ring-1 ring-black ring-opacity-5 overflow-hidden">
         <div className="p-4">
@@ -44,6 +38,17 @@ export function Notification({
           </div>
         </div>
       </Alert>
-    </Transition>
+    </div>
   );
 }
+
+/* <Transition
+className="fixed inset-0 flex items-end justify-center px-4 py-6 pointer-events-none sm:p-6 sm:items-start sm:justify-end"
+show={isOpen}
+enter="transform ease-out duration-300 transition"
+enterFrom="translate-y-2 opacity-0 sm:translate-y-0 sm:translate-x-2"
+enterTo="translate-y-0 opacity-100 sm:translate-x-0"
+leave="transition ease-in duration-100"
+leaveFrom="opacity-100"
+leaveTo="opacity-0"
+> */
